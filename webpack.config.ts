f587b5b96c4e1b7dbf9a562c4a8af533f45d4c70
Tyler Long <tyler4long@gmail.com>
2022-10-13 12:07:29 -0700

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,21 @@
+/* eslint-disable node/no-unpublished-import */
+import {Configuration} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: Configuration = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', 'tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
+
+export default config;
